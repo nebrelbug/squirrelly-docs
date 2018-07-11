d8f03c959c861e7eccd7678b8002fa800379d964
nebrelbug <nebrelbug@gmail.com>
2018-07-11 07:07:31 +0100

    fixed path to Squirrelly script

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -88,7 +88,7 @@ const siteConfig = {
   },
 
   // Add custom scripts here that would be placed in <script> tags
-  scripts: ['https://rawgit.com/nebrelbug/squirrelly/master/index.js'],
+  scripts: ['https://rawgit.com/nebrelbug/squirrelly/master/squirrelly.js'],
 
   /* On page navigation for the current documentation page */
   onPageNav: 'separate',
